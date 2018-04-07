ec9195fa74d0239345bfc14b866c9c487d39f127
Pranav <pranav.manik@gmail.com>
2018-04-07 14:21:46 -0400

    PrintMess

---
+++ PrintMessage.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintMessage : MonoBehaviour {
+	public void PrintActivateMessage(){
+		print("Activated");
+	}
+
+	public void PrintDeactivateMessage(){
+		print("Deactivated");
+	}
+}
